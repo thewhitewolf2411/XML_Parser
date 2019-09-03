e533813f9d05655ba01f20f582db5da3b0da179b
thewhitewolf2411 <39027423+thewhitewolf2411@users.noreply.github.com>
2019-09-04 00:20:41 +0200

    Add project files.

---
+++ XML_Parser/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Xml;
+using Microsoft.Win32;
+
+namespace XML_Parser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void LoadXml_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog OFN = new OpenFileDialog();
+            if (OFN.ShowDialog() == true) {
+
+                string sadrzaj = File.ReadAllText(OFN.FileName.ToString());
+                //xmlShow.Text = sadrzaj;
+
+                sadrzaj.Replace("°", "&#x000B0;");
+                sadrzaj.Replace();
+
+            }
+
+
+        }
+    }
+}
